8cbaadbf76b7352575d5fcffad38a6eb78efd1e8
Krotym <55706673+Krotym@users.noreply.github.com>
2019-11-05 17:04:48 +0200

    Update ms.sh

--- ms.sh
+++ ms.sh
@@ -5,6 +5,14 @@ mysql --protocol=socket -uroot  <<EOSQL
 
 
 CREATE DATABASE student;
+USE student;
+CREATE Table tovars (    
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  
+name VARCHAR(200) NOT NULL,  
+price VARCHAR(200) NOT NULL,    
+text VARCHAR(255) NOT NULL);
+
+
 
 CREATE USER 'misha' IDENTIFIED BY '1410261';
 GRANT ALL privileges ON student.* TO 'misha';
