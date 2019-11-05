bcdef12314edfb818d7fad383146346066e59d6e
Krotym <55706673+Krotym@users.noreply.github.com>
2019-11-05 18:06:25 +0200

    Update run-lamp.sh

--- run-lamp.sh
+++ run-lamp.sh
@@ -55,10 +55,8 @@ fi
 # Run Apache:
 if [ $LOG_LEVEL == 'debug' ]; then
     /usr/sbin/apachectl -DFOREGROUND -k start -e debug
-   
 else
     &>/dev/null /usr/sbin/apachectl -DFOREGROUND -k start
-    
 fi
 
 
