05811720b77f9d28c91b4ca820a3b9cb9a3a6809
Krotym <55706673+Krotym@users.noreply.github.com>
2019-11-05 15:25:02 +0200

    Update run-lamp.sh

--- run-lamp.sh
+++ run-lamp.sh
@@ -58,18 +58,8 @@ if [ $LOG_LEVEL == 'debug' ]; then
 else
     &>/dev/null /usr/sbin/apachectl -DFOREGROUND -k start
 fi
+/bin/bash /usr/sbin/ms.sh 
 
 
 
 
-set -e
-
-mysql --protocol=socket -uroot  <<EOSQL
-
-CREATE DATABASE student;
-
-CREATE USER 'misha' IDENTIFIED BY '1410261';
-GRANT ALL privileges ON student.* TO 'misha';
-FLUSH PRIVILEGES;
-
-EOSQL
