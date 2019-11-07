9aeeb35d767e45a43b33c7c6174f52fc8c66fd2d
Krotym <55706673+Krotym@users.noreply.github.com>
2019-11-07 16:25:49 +0200

    Update run-lamp.sh

--- run-lamp.sh
+++ run-lamp.sh
@@ -51,10 +51,7 @@ fi
 # Set PHP timezone
 /bin/sed -i "s/\;date\.timezone\ \=/date\.timezone\ \=\ ${DATE_TIMEZONE}/" /etc/php/7.0/apache2/php.ini
 # Run MariaDB
-/usr/bin/mysqld_safe --timezone=${DATE_TIMEZONE}&
-
-
-/usr/sbin/ms.sh
+/usr/bin/mysqld_safe --timezone=${DATE_TIMEZONE}&  &&\ /usr/sbin/ms.sh
 # Run Apache:
 if [ $LOG_LEVEL == 'debug' ]; then
     /usr/sbin/apachectl -DFOREGROUND -k start -e debug
